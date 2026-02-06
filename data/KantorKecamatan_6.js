fb393fc16d2c34ea12d90fd3c51eee324b4cb8ba
Barawangsa <1915041003@student.unm.ac.id>
2026-02-06 22:36:25 +0800

    Add files via upload

---
+++ data/KantorKecamatan_6.js
@@ -0,0 +1,1 @@
+var json_KantorKecamatan_6 = {"type":"FeatureCollection","name":"KantorKecamatan_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":0.0},"geometry":{"type":"Point","coordinates":[117.5334168,0.496]}}]}
